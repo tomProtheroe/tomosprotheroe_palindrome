e513d4807665d3cf70c59b4ede5d6d68eb9435a4
Tomos Protheroe <tomos@craftcourses.com>
2021-09-06 12:30:10 +0100

    Initialize repository

---
+++ lib/tomosprotheroe_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tomosprotheroe_palindrome/version"
+
+module TomosprotheroePalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
