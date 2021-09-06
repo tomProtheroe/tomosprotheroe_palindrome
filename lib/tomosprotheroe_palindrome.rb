0fd94ac6bb012248b190db184a1f71f5965981f2
Tomos Protheroe <tomos@craftcourses.com>
2021-09-06 15:31:27 +0100

    Finish working and refactored palindrome method

--- lib/tomosprotheroe_palindrome.rb
+++ lib/tomosprotheroe_palindrome.rb
@@ -2,7 +2,17 @@
 
 require_relative "tomosprotheroe_palindrome/version"
 
-module TomosprotheroePalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-zA-Z]/i).join.downcase
+    end
 end
